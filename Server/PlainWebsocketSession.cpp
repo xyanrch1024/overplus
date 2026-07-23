5d2cd8093226a398ac6a22c4c65ab3fe82313e77
xyanrch <yanrongcheng0@gmail.com>
2026-07-23 17:11:31 +0800

    Revert "fix: also add missing Upgrade header for HTTP/2 proxy"
    This reverts commit c6900b1410cfc8982a33ebb4bcdc11fd18bc76f4.

--- Server/PlainWebsocketSession.cpp
+++ Server/PlainWebsocketSession.cpp
@@ -25,9 +25,6 @@ void PlainWebsocketSession::on_http_header(beast::error_code ec, std::size_t)
     if (!http_request_.count(http::field::connection)) {
         http_request_.set(http::field::connection, "Upgrade");
     }
-    if (!http_request_.count(http::field::upgrade)) {
-        http_request_.set(http::field::upgrade, "websocket");
-    }
     beast::get_lowest_layer(upstream_socket).expires_never();
 
     upstream_socket.set_option(
